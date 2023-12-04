b8dee80ef8a8a3d1eaf1dbc205d5e5bc2ec54a07
anitaharyodi <haryodianita@gmail.com>
2023-12-04 14:07:56 +0700

    fix big img

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -4,7 +4,7 @@ import LogoSamping from "./img/LOGOSAMPING.png"
 import Slider1 from "./img/heroSlider/1.jpg"
 import Slider2 from "./img/heroSlider/2.jpg"
 import Slider3 from "./img/heroSlider/3.jpg"
-import HotelBuilding from "./img/hotelBuilding.jpg"
+import HotelBuilding from "./img/hotelbuilding.jpg"
 import BgProfile from './img/bgProfile.png'
 import UserProfile from './img/userProfile.png'
 import LogoAjaBg from './img/LogoAjaBg.png'
